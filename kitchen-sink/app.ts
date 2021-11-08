a6fdf0c6bdd69f9991c9cbbc78aaedec86202adb
unknown <richdanmac@gmail.com>
2021-11-08 14:28:25 -0600

    post-walkthrough edit of kitchen sink

--- kitchen-sink/app.ts
+++ kitchen-sink/app.ts
@@ -7,7 +7,7 @@ const numberOfStates: number = 50;
 // Sum of 5 + 4
 let sum: number = 5 + 4;
 // function sayHello displays alert "Hello World!"
-let sayHello = () => {
+let sayHello = (): void => {
     alert("Hello World!");
 }
 // calls the sayHello function
@@ -27,7 +27,7 @@ checkAge("John", 17)
 
 let myFavoriteVegetables: Array<string> = ["broccoli", "cauliflower", "leeks", "okra", "asparagus"]
 
-for (let i = 0; i < myFavoriteVegetables.length; i++) {
+for (let i: number = 0; i < myFavoriteVegetables.length; i++) {
     console.log(myFavoriteVegetables[i])
 };
 
@@ -40,7 +40,16 @@ let IDPet = (myPet: MyPet) => {
     console.log(`${myPet.name} is an ${myPet.breed}!`)
 };
 
+
 IDPet({name: 'Momo', breed: 'Australian Cattle Dog'})
+
+// OR
+// let myPet: MyPet = {
+//     name: 'Momo',
+//     breed: 'Australian Cattle Dog'
+// }
+
+// console.log(myPet.breed, myPet.name)
 
 let people: {name: string, age: number}[] = [
     {
@@ -65,7 +74,7 @@ let people: {name: string, age: number}[] = [
     }
 ];
 
-for (let i = 0; i < people.length; i++) {
+for (let i: number = 0; i < people.length; i++) {
     checkAge(people[i].name, people[i].age);
 
 }
@@ -74,7 +83,7 @@ let getLength = (word: string): number => {
     return word.length
 };
 
-let stringLength = getLength('Hello World!');
+let stringLength: number = getLength('Hello World!');
 
 if (stringLength % 2 == 0) {
     console.log('The world is nice and even!')
