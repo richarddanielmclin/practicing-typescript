879735f504b999f2b61d12515d66a6e98da1f0c0
unknown <richdanmac@gmail.com>
2021-11-08 14:32:33 -0600

    post-walkthrough edit of kitchen sink

--- kitchen-sink/app.js
+++ kitchen-sink/app.js
@@ -1,42 +1,44 @@
+"use strict";
+exports.__esModule = true;
 // String of my name
-let myName = "Richard";
+var myName = "Richard";
 // Number of US states
-const numberOfStates = 50;
+var numberOfStates = 50;
 // Sum of 5 + 4
-let sum = 5 + 4;
+var sum = 5 + 4;
 // function sayHello displays alert "Hello World!"
-function sayHello() {
+var sayHello = function () {
     alert("Hello World!");
-}
+};
 // calls the sayHello function
 sayHello();
-
-function checkAge(name, age) {
+var checkAge = function (name, age) {
     console.log(name, age);
     if (age < 21) {
-        alert("Sorry " + name + ", you aren't old enough to view this page!")
+        alert("Sorry " + name + ", you aren't old enough to view this page!");
     }
+};
+checkAge("Charles", 21);
+checkAge("Abby", 27);
+checkAge("James", 18);
+checkAge("John", 17);
+var myFavoriteVegetables = ["broccoli", "cauliflower", "leeks", "okra", "asparagus"];
+for (var i = 0; i < myFavoriteVegetables.length; i++) {
+    console.log(myFavoriteVegetables[i]);
 }
-
-checkAge("Charles", 21)
-checkAge("Abby", 27)
-checkAge("James", 18)
-checkAge("John", 17)
-
-let myFavoriteVegetables = ["broccoli", "cauliflower", "leeks", "okra", "asparagus"]
-
-for (let i = 0; i < myFavoriteVegetables.length; i++) {
-    console.log(myFavoriteVegetables[i])
+;
+;
+var IDPet = function (myPet) {
+    console.log(myPet.name + " is an " + myPet.breed + "!");
 };
-
-let myPet = {
-    name: "Momo",
-    breed: "Australian Cattle Dog"
-};
-
-console.log(myPet.name, myPet.breed);
-
-let people = [
+IDPet({ name: 'Momo', breed: 'Australian Cattle Dog' });
+// OR
+// let myPet: MyPet = {
+//     name: 'Momo',
+//     breed: 'Australian Cattle Dog'
+// }
+// console.log(myPet.breed, myPet.name)
+var people = [
     {
         name: "Leah",
         age: 22
@@ -58,20 +60,17 @@ let people = [
         age: 27
     }
 ];
-
-for (let i = 0; i < people.length; i++) {
+for (var i = 0; i < people.length; i++) {
     checkAge(people[i].name, people[i].age);
-
 }
-
-function getLength(word) {
-    return word.length
+var getLength = function (word) {
+    return word.length;
 };
-
-let stringLength = getLength('Hello World!');
-
+var stringLength = getLength('Hello World!');
 if (stringLength % 2 == 0) {
-    console.log('The world is nice and even!')
-} else {
-    console.log('The world is an odd place!')
-};
+    console.log('The world is nice and even!');
+}
+else {
+    console.log('The world is an odd place!');
+}
+;
